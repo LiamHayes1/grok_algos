0c90376710904b3ffb2c6ece07416dd11e008306
LiamHayes1 <liamhayes@live.ie>
2022-03-24 14:42:22 +0000

    more recursion ch4 excercises

--- recusion1.py
+++ recusion1.py
@@ -4,4 +4,17 @@ def sum(arr):
         return 0
     return arr[0] + sum(arr[1:])
 
-print(sum([1,2,3,4]))
+def count(arr):
+    if (arr == []):
+        return 0
+    return 1 + count(arr[1:])
+
+def max(list):
+    if len(list) == 2:
+        return list[0] if list[0] > list[1] else list[1]
+    sub_max = max(list[1:])
+    return list[0] if list[0] > sub_max else sub_max
+
+print('sum: ' + str(sum([1,2,3,4])))
+print('count: ' + str(count([1,2,3,4])))
+print('max: ' + str(max([1,2,6,3,4])))
